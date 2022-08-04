294c4e2921331fdfc0fa62845a732d9db2075880
Yamaan <yamaan93@gmail.com>
2022-08-04 13:45:32 -0400

    Add playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -11,7 +11,7 @@ function getComputerChoice(){
         response = "paper";
     }
     
-    else if (dice == 2){
+    else    if (dice == 2){
         response = "scissors";
     }
     else{
@@ -21,4 +21,39 @@ function getComputerChoice(){
     return response;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection = getComputerChoice()){
+    let outcome = "error"; //win or lose or error by default
+    playerSelection = playerSelection.toLowerCase(); //normalize player input incase of capitals
+    
+
+    if(playerSelection==computerSelection){
+        outcome = "tie!"
+
+    }
+    else {
+        if(playerSelection == "rock" && computerSelection == "scissors"){
+            outcome = "you win, rock beats scrissors";
+        }
+        else if (playerSelection == "rock"){
+            outcome = "you lose! paper beats rock";
+        }
+    
+        if(playerSelection == "paper" && computerSelection == "rock"){
+            outcome = "you win, paper beats rock";
+        }
+        else if (playerSelection == "paper"){
+            outcome = "you lose! scissors beat paper";
+        }
+    
+        if(playerSelection == "scissors" && computerSelection == "paper"){
+            outcome = "you win, scissors beats paper";
+        }
+        else if (playerSelection == "scissors"){
+            outcome = "you lose! rock beats scissors";
+        }
+    }
+
+    return outcome;
+    
+}
+
